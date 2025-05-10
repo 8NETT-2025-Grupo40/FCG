63d48c4f4ffb72d885fb8ed4840d254f230b873a
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-05-10 11:49:45 -0300

    [Issue #17] fix/test: Fix build errors and add property on validation.

--- UnitTests/User/UserTests.cs
+++ UnitTests/User/UserTests.cs
@@ -8,19 +8,22 @@ namespace UnitTests.User
         public void CreateUser_ShouldInitializePropertiesCorrectly()
         {
             // Arrange
+            const string expectedStatus = "active";
             Name name = new("João");
             Email email = new("joão@example.com");
             Password password = new("SenhaValida@123");
             UserRole role = UserRole.Admin;
+            var status = expectedStatus;
 
             // Act
-            FCG.Domain.Modules.Users.User user = new(name, email, password, role);
+            FCG.Domain.Modules.Users.User user = new(name, email, password, role, status);
 
             // Assert
             Assert.Equal(name, user.Name);
             Assert.Equal(email, user.Email);
             Assert.Equal(password, user.Password);
             Assert.Equal(role, user.Role);
+            Assert.Equal(expectedStatus, user.Status);
             Assert.True((DateTime.Now - user.CreateDate).TotalSeconds < 1);
         }
     }
