1c3cfe408f04872987f606e581079bf076d9be67
Luciano Castilho de Lima ∴ <luciano_castilho@hotmail.com>
2025-05-12 20:16:49 -0300

    feat: Nova migration para campo status

---
+++ FCG.Infrastructure/Migrations/20250511233007_AddStatusToUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCG.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "User");
+        }
+    }
+}
