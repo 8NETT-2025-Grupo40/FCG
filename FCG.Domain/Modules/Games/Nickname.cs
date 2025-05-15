b7131cb2bd4e1ee3c15e428adcc01df582dc72c3
anderson_mori <morianderson@hotmail.com>
2025-05-15 12:02:01 -0300

    #13 - Model de Game + PlayerProfile

---
+++ FCG.Domain/Modules/Games/Nickname.cs
@@ -0,0 +1,29 @@
+﻿using FCG.Domain.Common;
+
+namespace FCG.Domain.Modules.Games;
+
+public record Nickname
+{
+    public string Value { get; } = null!;
+    protected Nickname() { }
+
+    public Nickname(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new DomainException("Nickname is required.");
+        }
+
+        if (value.Length is < 3 or > 20)
+        {
+            throw new DomainException("Nickname must be between 3 and 20 characters long.");
+        }
+
+        // Validação de caracteres pode ser incluída
+        this.Value = value.Trim();
+    }
+
+    public override string ToString() => this.Value;
+
+    public static implicit operator Nickname(string value) => new Nickname(value);
+}
---
+++ FCG.Domain/Modules/Games/Price.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using FCG.Domain.Common;
+
+namespace FCG.Domain.Modules.Games;
+
+public record Price
+{
+    public decimal Value { get; }
+
+    public Price(decimal value)
+    {
+        if (value < 0)
+        {
+            throw new DomainException("Price cannot be negative.");
+        }
+
+        this.Value = decimal.Round(value, 2);
+    }
+
+    public override string ToString() =>
+        // Usa o formato inglês (EUA) de moeda ($1,234.56)
+        this.Value.ToString("C2", CultureInfo.GetCultureInfo("en-US"));
+
+    public static implicit operator Price(decimal value) => new Price(value);
+}
